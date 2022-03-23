f86df9e87deef889ad6657c6b3b94460eacb4764
webala <webala1001@gmail.com>
2022-03-23 20:26:51 +0300

    widgets component complete

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Widgets.css'
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  TwitterTimeLineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+  TwitterTimelineEmbed
+
+  } from 'react-twitter-embed';
 
 function Widgets() {
   return (
-    <div className='widgets'>Widgets</div>
+    <div className='widgets'>
+      <div className='widgets-input'>
+        <SearchIcon className='widgets-search-icon'/>
+        <input type='text' placeholder='Search Twitter' />
+      </div>
+
+      <div className='widgets-container'>
+        <h2>What's Happening?</h2>
+        <TwitterTweetEmbed tweetId={'858551177860055040'} />
+
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='cleverqazi'
+          options={{height: 400}} />
+          
+        <TwitterShareButton
+        url={'https://facebook.com/cleverprogrammer'}
+        options={{text: '#reactjs is awesome', via: 'cleverqazi'}} />
+      </div>
+    </div>
   )
 }
 
